f044d92221466634bf1e63f957dd8acddf3285fe
Hendi Santika <hendisantika@gmail.com>
2018-09-07 06:34:24 +0700

    #9 - Modify Buku Entity

--- src/main/java/com/hendisantika/buku/model/Buku.java
+++ src/main/java/com/hendisantika/buku/model/Buku.java
@@ -7,14 +7,10 @@ package com.hendisantika.buku.model;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.hendisantika.buku.JsonDateSerializer;
+
+import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  *
